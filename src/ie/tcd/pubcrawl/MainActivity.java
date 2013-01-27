bb96088d56a30ca1a6ab699a247820ca0b32146c
John Orr <johnorr123@gmail.com>
2013-01-27 21:52:21 +0000

    Testing
    Changes just for testing the Permanent Storage facilities

--- src/ie/tcd/pubcrawl/MainActivity.java
+++ src/ie/tcd/pubcrawl/MainActivity.java
@@ -6,21 +6,33 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends Activity {
+	String name;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        
+      
         Button gameMenu = (Button) findViewById(R.id.bGames);
         
         gameMenu.setOnClickListener(new View.OnClickListener() {
 			
 			public void onClick(View v) {
 				// TODO Auto-generated method stub
-				startActivity(new Intent("ie.tcd.pubcrawl.GAMEMENU"));
+				
+				EditText editText = (EditText) findViewById(R.id.eName);
+		        
+		        name = editText.getText().toString();
+		        
+		        PermStorage exampleVariable;
+		        exampleVariable = new PermStorage(getApplicationContext());
+		        
+		        exampleVariable.Store_User_Name(name);
+				
+		        startActivity(new Intent("ie.tcd.pubcrawl.GAMEMENU"));
 			}
 		});
     }
