b76bdbd8e05f53fb9876d90b8e6346836f7d1377
orrjl <orrjl@tcd.ie>
2013-01-31 20:56:42 +0000

    New better type of Permanent Storage
    Updates to the other files to come

--- src/ie/tcd/pubcrawl/CreateUserName.java
+++ src/ie/tcd/pubcrawl/CreateUserName.java
@@ -1,9 +1,7 @@
 package ie.tcd.pubcrawl;
 
 import android.app.Activity;
-import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -11,12 +9,10 @@ import android.widget.EditText;
 
 public class CreateUserName extends Activity {
 
-public final static String PREFS_NAME = "UserInfo";
-public static String userName;
-EditText getName;
+	public static String userName;
+	EditText getName;
 
     @Override
-
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.createusername);
@@ -24,28 +20,19 @@ EditText getName;
         getName = (EditText) findViewById(R.id.etUserName);
         Button saveName = (Button) findViewById(R.id.bSaveUserName);
         
-        SharedPreferences info = getSharedPreferences(PREFS_NAME, 0);
-        userName = info.getString("UserName", "NoName");
-
+        userName = PermStorage.Get_User_Name(this);
         if(userName!="NoName"){
-        startActivity(new Intent("ie.tcd.pubcrawl.MAINACTIVITY"));
-        finish();
+        	startActivity(new Intent("ie.tcd.pubcrawl.MAINACTIVITY"));
+        	finish();
         }
         
-        saveName.setOnClickListener(new View.OnClickListener() {
-			
+        saveName.setOnClickListener(new View.OnClickListener() {			
 			public void onClick(View v) {
-				// TODO Auto-generated method stub
 				userName = getName.getText().toString();
-        		SharedPreferences info = getSharedPreferences(PREFS_NAME, 0);
-        		SharedPreferences.Editor editor = info.edit();
-        		editor.putString("UserName", userName);
-        		editor.commit();
-        		
+        		PermStorage.Store_User_Name(userName, CreateUserName.this);        		
         		startActivity(new Intent("ie.tcd.pubcrawl.MAINACTIVITY"));
         		finish();
 			}
 		});
-
     }
 }
