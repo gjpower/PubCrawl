e884304042bc2fc1a728dbc0af5d4d97e8f0e8c7
orrjl <orrjl@tcd.ie>
2013-02-08 14:50:30 +0000

    Update src/ie/tcd/pubcrawl/CurrentCrawls.java

--- src/ie/tcd/pubcrawl/CurrentCrawls.java
+++ src/ie/tcd/pubcrawl/CurrentCrawls.java
@@ -5,12 +5,18 @@ import android.os.Bundle;
 
 public class CurrentCrawls extends Activity{
 	
+	String crawlData;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 		
 		setContentView(R.layout.currentcrawls);
+		
+		TextView textView = (TextView) findViewById(R.id.Crawl_Data);
+		crawlData = PermStorage.Get_Crawl_Data(this);
+		textView.setText(crawlData);
 	}
 
 }
