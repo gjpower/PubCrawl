6f69596e38982387c55f1df68542e59405321c5d
orrjl <orrjl@tcd.ie>
2013-02-08 14:52:33 +0000

    Update src/ie/tcd/pubcrawl/CurrentCrawls.java

--- src/ie/tcd/pubcrawl/CurrentCrawls.java
+++ src/ie/tcd/pubcrawl/CurrentCrawls.java
@@ -14,6 +14,7 @@ public class CurrentCrawls extends Activity{
 		
 		setContentView(R.layout.currentcrawls);
 		
+		//Crawl Data set to "No Crawls" on install of app
 		TextView textView = (TextView) findViewById(R.id.Crawl_Data);
 		crawlData = PermStorage.Get_Crawl_Data(this);
 		textView.setText(crawlData);
