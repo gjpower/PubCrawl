1556d68fecb82bd602e29345d092119e6ebf82c7
Graeme Power <gjpower@tcd.ie>
2013-03-21 23:31:44 +0000

    Implemented Downloading images to gallery

--- src/ie/tcd/pubcrawl/WebImage.java
+++ src/ie/tcd/pubcrawl/WebImage.java
@@ -1,31 +1,52 @@
 package ie.tcd.pubcrawl;
 
+import java.io.BufferedInputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.net.URLConnection;
+
+import org.apache.http.util.ByteArrayBuffer;
+
+import android.annotation.TargetApi;
 import android.app.Activity;
+import android.app.DownloadManager;
 import android.content.Intent;
+import android.media.MediaScannerConnection;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
+import android.widget.Toast;
 
 public class WebImage extends Activity {
 	// implemented this so the user isn't leaving the program to load up a browser to simple view the image
 	// could add ability to save the image to your gallery from the menu item
 
 	private WebView webArea;
+	private String urlString;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_web_image);
+
 		
 		Intent prevIntent = getIntent();
-		String url = prevIntent.getStringExtra("url");
+		urlString = prevIntent.getStringExtra("url");
 		
 		webArea = (WebView) findViewById(R.id.webArea);
 		
 		
 		try { 
-			webArea.loadUrl(url);	//don't even know if this returns an exception on failure
+			webArea.loadUrl(urlString);	//don't even know if this returns an exception on failure
 		} catch (Exception e) {		//just making sure it won't crash
             e.printStackTrace();
         }
@@ -45,5 +66,93 @@ public class WebImage extends Activity {
 		getMenuInflater().inflate(R.menu.activity_web_image, menu);
 		return true;
 	}
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+	    // Handle item selection		
+	    switch (item.getItemId()) {
+	        case R.id.save:
+	            saveImage();
+	            return true;
+	        default:
+	            return super.onOptionsItemSelected(item);
+	    }
+	}
+	
+	@TargetApi(Build.VERSION_CODES.HONEYCOMB)
+	private void saveImage() {
+		
+		File root = android.os.Environment.getExternalStorageDirectory();       	//get our sd card directory        
+
+        File savedir = new File ( root.getAbsolutePath() + "/Pictures/PubCrawl Images"); //create our directory if it doesn't exist
+        if(savedir.exists()==false) {
+             savedir.mkdirs();
+        }
+		
+		Uri uri = Uri.parse(urlString);
+        DownloadManager downloader = (DownloadManager) getSystemService(DOWNLOAD_SERVICE);
+        DownloadManager.Request request = new DownloadManager.Request(uri);
+        request.setDestinationInExternalPublicDir(Environment.DIRECTORY_PICTURES, 
+        					"PubCrawl Images/" + urlString.substring(urlString.lastIndexOf('/') +1));
+        //request.setNotificationVisibility(DownloadManager.Request.VISIBILITY_VISIBLE_NOTIFY_COMPLETED);
+        //request.allowScanningByMediaScanner();
+        downloader.enqueue(request);
+        
+		//below is manual method for downloading
+       /* 
+		try {
+	           URL url = new URL(urlString);
+	           String fileName = url.getFile();
+	           fileName = fileName.substring(fileName.lastIndexOf('/') + 1);	//remove all but the filename
+	           File file = new File(savedir, fileName);
+	           
+	           
+
+	           // Open a connection to that URL.
+	           URLConnection ucon = url.openConnection();
+
+	          
+	           //	Define InputStreams to read from the URLConnection.
+	           InputStream is = ucon.getInputStream();
+	           BufferedInputStream bis = new BufferedInputStream(is);
+
+
+	           // Read bytes to the Buffer until there is nothing more to read(-1).
+
+	           ByteArrayBuffer baf = new ByteArrayBuffer(5000);
+	           int current = 0;
+	           while ((current = bis.read()) != -1) {
+	              baf.append((byte) current);
+	           }
+
+
+	           // Write our buffer to the file
+	           FileOutputStream fos = new FileOutputStream(file);
+	           fos.write(baf.toByteArray());
+	           fos.flush();
+	           fos.close();
+	           
+	           String[] filePath = {file.getAbsolutePath()}; //doing this as below method requires a string array
+	           
+	           // below required to make images appear in the users gallery program
+	           MediaScannerConnection.scanFile(this, filePath, null, new MediaScannerConnection.OnScanCompletedListener()  
+	           	{ public void onScanCompleted(String path, Uri uri) {	//open the image in the gallery once it has been scanned into it
+//	        		Intent intent = new Intent();					//alternatively could just post a toast saying the image has been saved
+//	              intent.setAction(Intent.ACTION_VIEW);
+//	              intent.setDataAndType(uri,"image/*");
+//	              startActivity(intent);
+	      	} }  );
+	           
+	           Toast.makeText(this, "Image saved and added to Gallery" , Toast.LENGTH_SHORT).show(); //just giving a message instead of opening in gallery
+
+	   } catch (IOException e) {
+	       Log.e("DownloadManager", "Error: " + e);
+	   }
+	   */
+	   
+		
+	}
+	
+	
 
 }
