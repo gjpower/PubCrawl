93a26d985ef00bdd18dbedaea93a1a6154993d09
Siobhan Duignan <sduignan@lg12l35.scss.tcd.ie>
2013-04-04 10:18:01 +0100

    adding 3d dice

--- src/ie/tcd/pubcrawl/DiceGames.java
+++ src/ie/tcd/pubcrawl/DiceGames.java
@@ -2,32 +2,36 @@ package ie.tcd.pubcrawl;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.view.Display;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.support.v4.app.NavUtils;
 
 public class DiceGames extends Activity {
+	
+	DiceView diceView;
+	public static int Width, Height;
+	
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dice_games);
-        getActionBar().setDisplayHomeAsUpEnabled(true);
+        Display display = getWindowManager().getDefaultDisplay();
+        final int width = (display.getWidth());
+        final int height = (display.getHeight());
+        Width = width;
+        Height = height;
+        
+        diceView = new DiceView(this);
+        setContentView(diceView);
     }
 
+ 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.activity_dice_games, menu);
-        return true;
-    }
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case android.R.id.home:
-                NavUtils.navigateUpFromSameTask(this);
-                return true;
-        }
-        return super.onOptionsItemSelected(item);
+    public void onBackPressed() 
+    {
+    	super.onBackPressed();
     }
 
 }
