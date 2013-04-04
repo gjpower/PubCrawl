3dd69a84f1208d89c71814f508247a0e5b06e8c5
Conor O'Shea <osheac5@tcd.ie>
2013-04-04 11:37:46 +0100

    Fixed really annoying stupid bug in maps code ughhhh

--- src/ie/tcd/pubcrawl/CrawlMapOverview.java
+++ src/ie/tcd/pubcrawl/CrawlMapOverview.java
@@ -1,4 +1,3 @@
-/*
 package ie.tcd.pubcrawl;
 
 import android.content.Context;
@@ -15,74 +14,100 @@ import android.view.View;
 import android.widget.CheckBox;
 import android.widget.Toast;
 
+import com.google.android.gms.common.GooglePlayServicesNotAvailableException;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.GoogleMap.OnInfoWindowClickListener;
+import com.google.android.gms.maps.MapsInitializer;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.UiSettings;
 import com.google.android.gms.maps.model.BitmapDescriptor;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 public class CrawlMapOverview extends android.support.v4.app.FragmentActivity implements LocationListener{
-
+	
 	private int numpub = 2;
 	private Marker[] marker = new Marker[numpub]; 
     private GoogleMap mMap;
     private LocationManager locationManager;
     private String provider;
     private UiSettings mUiSettings;
+    public LatLngBounds bounds;
+    public final double latitude = 54;
+    public final double longitude = -5.9;
+    public LatLngBounds pubsnme;
+    public LatLng pubs = new LatLng(latitude, longitude);
+    public LatLng pubs1 = new LatLng(latitude, longitude);
     public LatLng myloc;
     public LatLng[] allpubs = new LatLng[numpub];
-    public CustomInfoAdapter cia = new CustomInfoAdapter(this);
-    private BitmapDescriptor icon;
-
+    private BitmapDescriptor icon, icon2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-     	setContentView(R.layout.third);
+     	setContentView(R.layout.activity_crawl_map_overview);
+     	Log.i("user", "on create");
         setUpMapIfNeeded();
         icon = BitmapDescriptorFactory.fromResource(R.drawable.icon);
+        icon2 = BitmapDescriptorFactory.fromResource(R.drawable.icon2);
+        Log.i("user", "map set up");
         getpublocations();
+        Log.i("user", "got locations");
         makeMarkers();
+        Log.i("user", "made markers");
         init();
-//        infoWindow=getLayoutInflater().inflate(R.layout.info_window_layout, null);
-        mMap.setOnInfoWindowClickListener(null);
-        //doSomething();  
+        Log.i("user", "init");
+        //doSomething();
+        
+        
           
-        } 
+        }
+    
+    
 
     private void makeMarkers() {
     	for(int i=0; i < numpub; i++){
+    		if(i==0){
+    			Marker tempmarker = mMap.addMarker(new MarkerOptions()
+      	      .position(allpubs[i])
+      	      .title("pub.title")
+      	      .snippet("pub.info and shiit")
+      		  .icon(icon2));
+      		marker[i] = tempmarker;
+      		marker[i].showInfoWindow();
+    		}
+    		else{
     		Marker tempmarker = mMap.addMarker(new MarkerOptions()
     	      .position(allpubs[i])
-    	      .title("Messrs Maguire")
-    	      .snippet("Touch for Directions")
+    	      .title("pub.title")
+    	      .snippet("pub.info and shiit")
     		  .icon(icon));
     		marker[i] = tempmarker;
     		marker[i].showInfoWindow();
+    		}
     	}
 		
 	}
 
 	private void getpublocations() {
-		//String strlat, strlng;
-		String[] testlat = new String[numpub];
-		String[] testlng = new String[numpub];
-		testlat[0] = "53.3439208";
-		testlng[0] = "-6.2607184";
-		testlat[1] = "53.347001626 ";
-		testlng[1] = "-6.258100569";
-		double templat, templng;
 		for(int i=0; i < numpub; i++){
-			//get strings from pub managment
-			//strlat = "53.3439208";
-			//strlng = "-6.2607184";
-			templat = Double.parseDouble(testlat[i]);
-			templng = Double.parseDouble(testlng[i]);
+			String strlat, strlng;
+			double templat, templng;
+			//get strings from pub management
+			Log.i("user", "for loop");
+			if(i==0){
+			strlat = "53.347001626";
+			strlng = "-6.258100569";
+			}
+			else{
+				strlat = "53.343057711";
+				strlng = "-6.258905232";
+			}
+			templat = Double.parseDouble(strlat);
+			templng = Double.parseDouble(strlng);
 			allpubs[i] = new LatLng(templat, templng);
 		}
 	}
@@ -104,15 +129,18 @@ public class CrawlMapOverview extends android.support.v4.app.FragmentActivity im
           if (location != null) {
             System.out.println("Provider " + provider + " has been selected.");
             onLocationChanged(location);}
+            mMap.moveCamera(CameraUpdateFactory.newLatLngBounds(pubsnme, 500, 500, 10));
             location = locationManager.getLastKnownLocation(provider);
             onLocationChanged(location);
-            
-            mMap.setOnInfoWindowClickListener(new OnInfoWindowClickListener() {
+           /* 
+            mMap.setOnMarkerClickListener(new OnMarkerClickListener() {
 
-				public void onInfoWindowClick(Marker arg0) {
+				@Override
+				public boolean onMarkerClick(Marker arg0) {
 					getDirections(arg0);
+					return false;
 				}
-            });
+            });*/
     }
     
     
@@ -125,6 +153,7 @@ public class CrawlMapOverview extends android.support.v4.app.FragmentActivity im
         provider = locationManager.getBestProvider(criteria, false);
         Location location = locationManager.getLastKnownLocation(provider);
         onLocationChanged(location);
+        mMap.moveCamera(CameraUpdateFactory.newLatLngBounds(pubsnme, 500, 500, 10));
     }
 
  
@@ -132,8 +161,8 @@ public class CrawlMapOverview extends android.support.v4.app.FragmentActivity im
         // Do a null check to confirm that we have not already instantiated the map.
         if (mMap == null) {
             // Try to obtain the map from the SupportMapFragment.
-            mMap = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map))
-                    .getMap();
+            SupportMapFragment fragment = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map));
+            mMap = fragment.getMap();
             // Check if we were successful in obtaining the map.
             if (mMap != null) {
                 setUpMap(52,-6);
@@ -146,21 +175,20 @@ public class CrawlMapOverview extends android.support.v4.app.FragmentActivity im
     	double lng = (double) (location.getLongitude());
     	myloc = new LatLng(lat, lng);
     	setUpMap(lat, lng);
+    	
+    	LatLngBounds temp = new LatLngBounds(new LatLng(lat, lng), pubs);
+    	pubsnme = temp;   
     	Log.i("user", "location changed");
-    	if(allpubs[0]!=null)
-        	mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(allpubs[0], 14));
-        else
-        	mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(myloc, 14));
+    	mMap.moveCamera(CameraUpdateFactory.newLatLngBounds(pubsnme, 500, 500, 10));
     	Log.i("user", "move camera");
-    	Toast.makeText(this, "latitude " + lat + " longditude " + lng, Toast.LENGTH_SHORT).show(); 
+    	Toast.makeText(this, " latitude " + lat + "longditude" + lng, Toast.LENGTH_SHORT).show(); 
     }
     
     
     private void setUpMap(double lat, double lng) {
-        mMap.setMapType(GoogleMap.MAP_TYPE_HYBRID);
+        mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
         mMap.setMyLocationEnabled(true);
         mUiSettings = mMap.getUiSettings();
-        mMap.setInfoWindowAdapter(cia);
     }
     
     private void getDirections(Marker mark){
@@ -192,4 +220,3 @@ public class CrawlMapOverview extends android.support.v4.app.FragmentActivity im
           Toast.LENGTH_SHORT).show();
     }
 }
-*/
